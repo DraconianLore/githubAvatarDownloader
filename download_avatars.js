e679ffb604d4a644223c66d0ddd1b753cfb47310
Steven Wing <steven.wing82@gmail.com>
2019-06-03 21:47:49 +0000

    getting usernames and avatar urls

--- download_avatars.js
+++ download_avatars.js
@@ -1,9 +1,19 @@
 var request = require('request');
+var secret = require('./secrets.js').GITHUB_TOKEN;
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-
+    var options = {
+        url: 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+        headers: {
+            'User-Agent': 'request',
+            'Authorization': 'token ' + secret
+        }
+    };
+    request(options, function(err, res, body) {
+        cb(err, body);
+    })
 }
 
 
@@ -12,6 +22,10 @@ function getRepoContributors(repoOwner, repoName, cb) {
 
 getRepoContributors('robbyrussell', 'oh-my-zsh', function(err, results) {
     console.log('errors:', err);
-    console.log('Results:', results);
+    let contribs = JSON.parse(results)
+    for (let people of contribs) {
+        console.log(people.login, people.avatar_url);
+    }
+    // console.log('Results:', contribs);
 
 });
