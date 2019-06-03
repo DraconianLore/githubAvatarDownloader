d037c44f49eae9f32e5348999b28db72644583e9
Steven Wing <steven.wing82@gmail.com>
2019-06-03 21:19:05 +0000

    hardcoded data in

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,17 @@
 var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+
+}
+
+
+
+
+
+getRepoContributors('robbyrussell', 'oh-my-zsh', function(err, results) {
+    console.log('errors:', err);
+    console.log('Results:', results);
+
+});
